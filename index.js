3ee3888c10fa7a496c6ce0cba817082d874ca1a3
Gripzz <99049290+Gripzz1@users.noreply.github.com>
2022-04-11 20:16:21 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const chalk = require("chalk");
+const Discord = require("discord.js");
+require("dotenv").config();
+
+const client = new Discord.Client({
+  intents: ["GUILDS", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
+  partials: ["GUILD_MEMBER", "CHANNEL", "REACTION", "USER"],
+});
+
+module.exports = client;
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.role = "954237695981408296";
+
+fs.readdirSync("./handler").forEach((file) => {
+  require(`./handler/${file}`);
+});
+
+client.login(process.env.token);
